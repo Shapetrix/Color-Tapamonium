053cd23a572a80fbd1119b39d8a2d251727d3a43
MRBATSHAPETRIX\Brandon Humfleet <shapetrix@gmail.com>
2019-07-08 12:00:41 -0500

    Title Movment
    working on title movement when the page loads and then moves out of screen when userBtn is clicked

--- js/mygame.js
+++ js/mygame.js
@@ -1,6 +1,18 @@
 var d3;
 document.addEventListener('DOMContentLoaded', function(event) {
-  d3.select('#userBtn').on("click", function(){alert('play')});
-  d3.select('#quitBtn').on("click", function(){alert('i quit')});
-
+  d3.select('#userBtnD3ID').on("click", function(){
+    d3.select('#colorTitleD3ID')
+    .attr('transform', 'translate(' + 65 + ',' + 35 + ')')
+    .transition('#colorTitleD3ID')
+    .duration(1200)
+    .attr('transform', 'translate(' + 65 + ',' + -300 + ')');
+  });
+  d3.select('#quitBtnD3ID').on("click", function(){alert('i quit')});
+  d3.select('#colorTitleD3ID').attr('transform', 'translate(' + 65 + ',' + -300 + ')').transition('#colorTitleD3ID')
+  .duration(1200)
+  .attr('transform', 'translate(' + 65 + ',' + 35 + ')');
 });
+
+function startGame(){
+  alert('play');
+}
