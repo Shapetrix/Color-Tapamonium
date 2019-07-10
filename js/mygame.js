f0e106539c72c82d6519881e4545e963012d6ca1
MRBATSHAPETRIX\Brandon Humfleet <shapetrix@gmail.com>
2019-07-10 13:11:35 -0500

    added let colorTM
    restructuring code so we can use the dot notation easier for d3.select

--- js/mygame.js
+++ js/mygame.js
@@ -1,19 +1,41 @@
 var d3;
 document.addEventListener('DOMContentLoaded', function(event) {
-  d3.select('#userBtnD3ID').on("click", function(){
+  /*
+  d3.xml('img/playBtn.svg').mimeType('image/svg+xml').get(function(error,xml){
+    if (error) throw error;
+    d3.select('#colorTitleD3ID').node().appendChild(xml.documentElement);
+  });
+  */
+  d3.select('#userBtnD3ID').on("click", colorTM.startGame);
+  d3.select('#quitBtnD3ID').on("click", colorTM.quitGame);
+  d3.select('#colorTitleD3ID')
+  .attr('transform', 'translate(' + 65 + ',' + -300 + ')')
+  .transition('#colorTitleD3ID')
+  .duration(1200)
+  .attr('transform', 'translate(' + 65 + ',' + 35 + ')');
+});
+/*
+function startGame(){
+  //alert('play');
+  d3.select('#colorTitleD3ID')
+  .attr('transform', 'translate(' + 65 + ',' + 35 + ')')
+  .transition('#colorTitleD3ID')
+  .duration(1200)
+  .attr('transform', 'translate(' + 65 + ',' + -300 + ')');
+}
+*/
+
+let colorTM = {
+  startGame(){
+    //alert('play');
     d3.select('#colorTitleD3ID')
     .attr('transform', 'translate(' + 65 + ',' + 35 + ')')
     .transition('#colorTitleD3ID')
     .duration(1200)
     .attr('transform', 'translate(' + 65 + ',' + -300 + ')');
-  });
-  d3.select('#quitBtnD3ID').on("click", function(){alert('i quit')});
-  d3.select('#colorTitleD3ID').attr('transform', 'translate(' + 65 + ',' + -300 + ')')
-  .transition('#colorTitleD3ID')
-  .duration(1200)
-  .attr('transform', 'translate(' + 65 + ',' + 35 + ')');
-});
+  },
+  quitGame(){
+    alert('i quit');
+  }
 
-function startGame(){
-  alert('play');
-}
+};
